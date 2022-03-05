d329d499afad3c521f0ff30d23c5ab6b1d863117
Tim Givois <tim.givois.mendez@gmail.com>
2022-03-05 12:50:18 -0400

    Add client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import "./App.css";
+
+const GET_BOOK = gql`
+  query GetBook($id: ID!) {
+    book(id: $id) {
+      title
+      published
+    }
+  }
+`;
+
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      title
+      published
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_BOOK, { variables: { id: 2 } });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
+  return <div>{data.book.title}</div>;
 }
 
 export default App;
